c0d2fbfbfefc10ca05df8321ebf27351ae5ef201
Oshawhat <alannobledalL@gmail.com>
2024-02-05 09:17:41 -0600

    create door script

---
+++ Scripts/Door.gd
@@ -0,0 +1,19 @@
+extends TileMap
+
+export var OpenOffset = Vector2(0, 0)
+export var Hide = true
+export var StartClosed = false
+
+onready var startPos = self.position
+onready var openPos = self.position + OpenOffset
+
+func trigger(close: bool):
+	self.position = (startPos if close else openPos)
+	
+	if !Hide: return
+	self.visible = close
+
+func _ready():
+	print(startPos)
+	print(openPos)
+	self.trigger(StartClosed)
