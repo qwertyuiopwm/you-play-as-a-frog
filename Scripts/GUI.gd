e4249e3c9e5141083ec348ff004d268526622288
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-02-15 11:27:43 -0600

    Functional mana wheel & Sync mana spot to selected spell

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -2,9 +2,12 @@ extends Control
 
 onready var spellWheel = $CanvasLayer/spellwheel
 onready var hpPanel = $CanvasLayer/top_bar/stats_bg/VBoxContainer/stats/HP_Canvas/Panel
+onready var spellSpot = $CanvasLayer/spellspot
+onready var manaPanel = spellSpot.get_node("mana")
 onready var Player = get_parent()
-onready var vp = get_viewport()
+onready var vp = get_viewport() 
 onready var maxHpSize = hpPanel.rect_size.x
+onready var maxManaSize = manaPanel.rect_size.x
 
 var SpellWheelPositions = []
 
@@ -12,12 +15,7 @@ func magnitude(vec: Vector2):
 	return sqrt(pow(vec.x, 2)+pow(vec.y, 2))
 
 func _ready():
-	for node in spellWheel.get_children():
-		if node.get_class() != "Position2D":
-			continue
-		if node.name.find("Spell") == -1:
-			continue
-		SpellWheelPositions.push_back(node)
+	SpellWheelPositions = generateWheel(len(Player.spells))
 	var i = 0
 	for packedSpell in Player.spells:
 		i+=1
@@ -26,24 +24,66 @@ func _ready():
 		spellIcon.texture = load(spell.SpellIcon)
 		
 
+func generateWheel(spellSlots: int):
+	var positions = []
+	#if spellSlots <= 1:
+	#	positions.push_back(Vector2(0,0))
+	#	return positions
+	
+	var wheel = spellWheel.get_node("wheel_bg")
+	var baseLine = spellWheel.get_node("line_base")
+	var baseIcon = spellWheel.get_node("icon_base")
+	var wheelRadius = wheel.rect_size.x / 2
+	var distanceFromCenter = 0.65
+	var sectorAngle = (360 / spellSlots)
+	
+	for i in spellSlots:
+		# Generate seperator line
+		var newLine = baseLine.duplicate()
+		var lineAngle = sectorAngle * i
+		newLine.rect_rotation = lineAngle
+		newLine.visible = true
+		spellWheel.add_child(newLine)
+		# Generate spell icon
+		# MAAAATTHHHHHHH RAAAAAAHHHHHHHHHH
+		var newIcon = baseIcon.duplicate()
+		var iconX = (cos(deg2rad(lineAngle+sectorAngle/2)) * wheelRadius * distanceFromCenter) - (newIcon.rect_size.x/2)
+		var iconY = (sin(deg2rad(lineAngle+sectorAngle/2)) * wheelRadius * distanceFromCenter) - (newIcon.rect_size.y/2)
+		var iconPosition = Vector2(iconX, iconY)
+		
+		newIcon.rect_position = iconPosition
+		newIcon.visible = true
+		newIcon.name = "Spell" + String(i + 1) + "Icon"
+		print(newIcon.name)
+		spellWheel.add_child(newIcon)
+		positions.push_back(iconPosition)
+	return positions
+
 func _process(_delta):
 	var newHealthX = (maxHpSize/Player.max_health)*Player.health
-	var newRectSize = Vector2(newHealthX, 16)
-	hpPanel.set_size(newRectSize)
+	var newHpRectSize = Vector2(newHealthX, hpPanel.rect_size.y)
+	hpPanel.set_size(newHpRectSize)
+	
+	var newManaX = (maxManaSize/Player.max_mana)*Player.mana
+	var newManaRectSize = Vector2(newManaX, manaPanel.rect_size.y)
+	manaPanel.set_size(newManaRectSize)
 	
 	spellWheel.visible = Input.is_action_pressed("select_spell")
 	
 	if spellWheel.visible:
 		var mousePos = vp.get_mouse_position()
-		var selectedNode
 		var lowestMag = INF
-		for node in SpellWheelPositions:
-			var newMag = magnitude(node.global_position+Vector2(400,400)-mousePos)
+		var selectedIndex: int
+		var i = 0
+		for vector in SpellWheelPositions:
+			i+=1
+			var newMag = magnitude(vector+Vector2(400,400)-mousePos)
 			if newMag < lowestMag:
-				selectedNode = node
+				selectedIndex = i
 				lowestMag = newMag
-		var selectedSpell = Player.spells[selectedNode.SpellIndex]
+		var selectedSpell = Player.spells[(selectedIndex-1)]
 		if selectedSpell:
 			Player.selected_spell = selectedSpell
-			$CanvasLayer/spellwheel/arow.rect_rotation = selectedNode.ArrowRotation
+			$CanvasLayer/spellwheel/arrow.rect_rotation = (len(Player.spells)/360)*selectedIndex
+			spellSpot.get_node("spellicon").texture = load(selectedSpell.instance().SpellIcon)
 		
