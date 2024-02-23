217cede0f561ad33511a3939523ef37130193b93
Oshawhat <alannobledalL@gmail.com>
2024-02-23 07:25:51 -0600

    update ant

--- Scripts/Ant.gd
+++ Scripts/Ant.gd
@@ -2,6 +2,8 @@ extends "res://Scripts/Enemy.gd"
 
 
 export var CIRCLE_RADIUS = 100
+export var BITE_DAMAGE = 10
+export var TARGET_COMFORT_RADIUS = 10
 
 
 enum states {
@@ -25,7 +27,12 @@ func _physics_process(delta):
 		states.STILL:
 			$AnimatedSprite.play("still")
 		states.CIRCLE:
+			$AnimatedSprite.play("walk")
 			target_pos = get_circle_position(randomnum, CIRCLE_RADIUS)
+			move(target_pos, delta)
+			if true: pass
+		states.SWARM:
+			move(target_player.global_position, delta)
 
 
 func get_state():
@@ -34,15 +41,28 @@ func get_state():
 	
 	if state == states.BITING:
 		return states.BITING
+		
+	if (global_position.distance_to(target_player.global_position) <= ATTACK_RANGE):
+		$AnimatedSprite.play("ready_attack")
+		return states.BITING
 	
-	if (global_position.distance_to(target_pos) <= ATTACK_RANGE):
-		return states.BITE
-
-	if (global_position.distance_to(target_pos) <= TARGET_RANGE):
+	if state == states.SWARM:
 		return states.SWARM
+		
+	if state == states.CIRCLE and global_position.distance_to(target_pos) <= TARGET_COMFORT_RADIUS:
+		return states.SWARM
+		
+	if (global_position.distance_to(target_player.global_position) <= TARGET_RANGE):
+		return states.CIRCLE
 
 
 func animation_finished():
 	match $AnimatedSprite.animation:
-		"bite":
-			pass
+		"ready_attack":
+			for body in $HitCollider.get_overlapping_bodies():
+				if !body.is_in_group("Player"): continue
+				
+				body.Hurt(BITE_DAMAGE)
+			$AnimatedSprite.play("hit")
+		"hit":
+			state = states.STILL
