43429c6ea676f6135b9f9f2f6604aed2eaab90a6
Oshawhat <alannobledalL@gmail.com>
2024-02-23 08:33:37 -0600

    remove signal

--- Scripts/ProcureCondiment.gd
+++ Scripts/ProcureCondiment.gd
@@ -3,10 +3,6 @@ extends "res://Scripts/Spell.gd"
 var condiment_residue = preload("res://Spells/Spell Extras/CondimentResidue.tscn")
 
 
-func _on_Area2D_body_entered(body):
-	hit(body)
-
-
 func on_settle():
 	var residue = condiment_residue.instance()
 	residue.global_position = global_position
