91db3cc71862586d49e0e25e8d34a47761ec9515
Oshawhat <alannobledalL@gmail.com>
2024-02-29 16:45:50 -0600

    fix trigger_on_start and door

--- Scripts/Triggerable.gd
+++ Scripts/Triggerable.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
-export var TRIGGER_ON_START = false
 export var State = false
-
-
-func _ready():
-	if not TRIGGER_ON_START: return
-	trigger(State)
 
 
 func trigger(trigger: bool):
