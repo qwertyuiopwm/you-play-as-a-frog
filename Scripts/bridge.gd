8782650b8f479b21526414baae57ccec8950c9ff
Oshawhat <alannobledalL@gmail.com>
2024-03-01 12:41:15 -0600

    change to a ladder when player brings one to it

--- Scripts/bridge.gd
+++ Scripts/bridge.gd
@@ -1,12 +1,13 @@
 extends Node2D
-class_name Ladder, "res://Items/Ladder.tscn"
 
 
 
 
 func _on_Area2D_body_entered(body):
-	if not body.is_in_group("player"): return
-	if not body.held_big_item is Ladder: return
+	if not body.is_in_group("Player"): return
+	var item = body.held_big_item
+	if not (item is Ladder): return
 	
 	body.held_big_item = null
+	item.queue_free()
 	$TileMap.set_cell(0, 0, 4)
