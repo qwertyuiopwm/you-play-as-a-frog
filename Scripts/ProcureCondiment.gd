bbdd7303638f3fbdc72be53228bd40bd28e7a5f9
Oshawhat <alannobledalL@gmail.com>
2024-03-08 11:56:55 -0600

    add animation to procure condiment

--- Scripts/ProcureCondiment.gd
+++ Scripts/ProcureCondiment.gd
@@ -5,4 +5,18 @@ var condiment_residue = preload("res://Spells/Spell Extras/CondimentResidue.tscn
 
 func on_settle():
 	var residue = condiment_residue.instance()
+	get_parent().add_child(residue)
 	residue.global_position = global_position
+	print(residue)
+	
+
+
+func animation_finished():
+	match $AnimatedSprite.animation:
+		"start":
+			$AnimatedSprite.play("default")
+
+
+func on_start():
+	$AnimatedSprite.connect("animation_finished", self, "animation_finished")
+	$AnimatedSprite.play("start")
