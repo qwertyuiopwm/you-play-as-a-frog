f95e20b9ca07242785627c541c74215ffee330e4
Oshawhat <alannobledalL@gmail.com>
2024-03-10 22:58:43 -0500

    move script to scripts folder

--- Scripts/CondimentResidue.gd
+++ Scripts/CondimentResidue.gd
@@ -14,3 +14,9 @@ func _process(delta):
 	counter += delta
 	if counter >= DURATION:
 		queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if body is Entity:
+		body.Afflict(Effects.slippy, 5)
+		queue_free()
