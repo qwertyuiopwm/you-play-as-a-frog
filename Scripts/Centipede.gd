30c9c0990ef99f2efcfc3f4713df7a2e930e4ad5
Oshawhat <alannobledalL@gmail.com>
2024-03-12 14:33:28 -0500

    set segment pos on ready

--- Scripts/Centipede.gd
+++ Scripts/Centipede.gd
@@ -6,6 +6,10 @@ var next_segment
 onready var next_segment_back = next_segment.get_node("Back")
 
 
+func _ready():
+	global_position = next_segment_back.global_position - $Front.position.rotated(rotation)
+
+
 func _physics_process(_delta):
 	
 	rotation = global_position.angle_to_point(next_segment_back.global_position)
