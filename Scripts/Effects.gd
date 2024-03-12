31b8acfd877f2f28723c64633fc974429442252d
Oshawhat <alannobledalL@gmail.com>
2024-03-12 14:25:28 -0500

    add stunned effect to effects

--- Scripts/Effects.gd
+++ Scripts/Effects.gd
@@ -3,3 +3,4 @@ class_name Effects
 
 const poison = preload("res://Effects/Poison.tscn")
 const slippy = preload("res://Effects/Slippy.tscn")
+const stunned = preload("res://Effects/Stunned.tscn")
--- Scripts/Stunned.gd
+++ Scripts/Stunned.gd
@@ -1,4 +1,9 @@
 extends "res://Scripts/Effect.gd"
 
-func on_affect(entity, delta):
+
+func on_affect(entity, _delta):
 	entity.can_move = false
+
+
+func on_effect_removed(entity):
+	entity.can_move = true
