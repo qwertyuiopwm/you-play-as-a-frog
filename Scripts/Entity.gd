aae181573a7464482801da1d38706fb36087b6a5
Oshawhat <alannobledalL@gmail.com>
2024-03-12 14:25:11 -0500

    add immunities list to entity

--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -2,11 +2,14 @@ extends KinematicBody2D
 class_name Entity
 
 
+export(Array, PackedScene) var IMMUNITIES = []
+
 var sliding = false
 var can_move = true
 
 
 func Afflict(effect, duration: float=-1):
+	if effect in IMMUNITIES: return
 	$EffectManager.Afflict(effect, duration)
 
 func Cure(effect):
