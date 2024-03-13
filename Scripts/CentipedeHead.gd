eb7ee6ee85c22128344e373f0cdb81a6279b5b4a
Oshawhat <alannobledalL@gmail.com>
2024-03-13 09:39:26 -0500

    add damage logic, add tail at the end, use move func from enemy for steering

--- Scripts/CentipedeHead.gd
+++ Scripts/CentipedeHead.gd
@@ -1,30 +1,44 @@
 extends "res://Scripts/Enemy.gd"
 
 
+export var CONTACT_DAMAGE = 20
 export var NUM_SEGMENTS = 3
 
 var segment_scene = preload("res://Enemies/Centipede/CentipedeSegment.tscn")
+var tail_scene = preload("res://Enemies/Centipede/CentipedeTail.tscn")
 
+var segments = []
 
 func _ready():
 	var next_segment = self
 	for x in NUM_SEGMENTS:
 		var segment = segment_scene.instance()
-		
+		segments.push_back(segment)
 		segment.next_segment = next_segment
-		get_parent().call_deferred("add_child", segment)
 		
 		next_segment = segment
+	
+	var tail = tail_scene.instance()
+	segments.push_back(tail)
+	tail.next_segment = next_segment
+	
+	for segment in segments:
+		segment.head = self
+		get_parent().call_deferred("add_child", segment)
+		segment.get_node("HitCollider").connect("body_entered", self, "body_entered")
 
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var players = get_tree().get_nodes_in_group("Player")
 	
 	#if not players: return
 	var target = players[0]
 	
-	rotation = global_position.angle_to_point(target.global_position)
 	
 	var target_pos = target.global_position
-	var velocity = global_position.direction_to(target_pos) * SPEED
-	var _ret = move_and_slide(velocity)
+	move(target_pos, delta)
+
+
+func _body_entered(body):
+	if body.is_in_group("Player"):
+		body.hurt(CONTACT_DAMAGE)
