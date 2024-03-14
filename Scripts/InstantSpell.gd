1d694adf81e47b6113b0e2f9097ff2137108e672
Oshawhat <alannobledalL@gmail.com>
2024-03-14 09:31:13 -0500

    take mana from caster

--- Scripts/InstantSpell.gd
+++ Scripts/InstantSpell.gd
@@ -3,10 +3,11 @@ extends "res://Scripts/Spell.gd"
 
 func try_cast(caster):
 	if caster.mana >= MANA_COST:
+		caster.mana -= MANA_COST
 		on_cast(caster)
 	
 	queue_free()
 
 
-func on_cast(caster):
+func on_cast(_caster):
 	pass
