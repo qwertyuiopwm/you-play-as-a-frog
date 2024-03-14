4cd4cd8a92d0958bc94469936ffb969c6c4e5a46
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-03-14 13:37:37 -0500

    add spell trigger

---
+++ Scripts/Spells.gd
@@ -0,0 +1,12 @@
+class_name Spells
+
+
+const Procure_Condiment = preload("res://Spells/ProcureCondiment.tscn")
+const Dev_Beam = preload("res://Spells/DevBeam.tscn")
+const Shake = preload("res://Spells/Shake.tscn")
+
+const AllSpells = {
+	Procure_Condiment = Procure_Condiment,
+	Dev_Beam = Dev_Beam,
+	Shake = Shake,
+}
