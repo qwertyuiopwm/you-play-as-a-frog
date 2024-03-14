67c95044919f1058d961333509c8b73081b26aa3
Oshawhat <alannobledalL@gmail.com>
2024-03-14 06:19:08 -0500

    finish shake spell

--- Spells/Shake.gd
+++ Spells/Shake.gd
@@ -1,16 +1,32 @@
-extends Node2D
+extends "res://Scripts/InstantSpell.gd"
 
 
-export var radius = 25
+onready var radius = $Area2D/CollisionShape2D.shape.radius
 
 
-func _ready():
-	print($Area2D/CollisionShape2D.shape.radius)
-	on_cast()
-
-
-func on_cast():
-	print("printing colliding bodies...")
-	print($Area2D.get_overlapping_bodies())
-	print("printing colliding areas...")
-	print($Area2D.get_overlapping_areas())
+func on_cast(caster):
+	global_position = caster.global_position
+	
+	for body in $Area2D.get_overlapping_bodies():
+		if not body is TileMap:
+			continue
+			
+		if body.is_in_group("ground"):
+			continue
+		
+		var tiles = body.get_used_cells()
+		
+		for tile_pos in tiles:
+			var tile_id = body.get_cellv(tile_pos)
+			
+			if tile_id == -1:
+				continue
+			
+			if not "(shake)" in body.tile_set.tile_get_name(tile_id):
+				continue
+				
+			var tile_global_pos = body.map_to_world(tile_pos)
+			if global_position.distance_to(tile_global_pos) > radius:
+				continue
+			
+			body.set_cellv(tile_pos, -1)
