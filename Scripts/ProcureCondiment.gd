e7218d9564a50b5bdd5c192c50e1c906a3eaf5e1
Oshawhat <alannobledalL@gmail.com>
2024-03-15 10:41:07 -0500

    add self_cast to slippy

--- Scripts/ProcureCondiment.gd
+++ Scripts/ProcureCondiment.gd
@@ -2,6 +2,7 @@ extends "res://Scripts/BouncingSpell.gd"
 
 
 export var SLIPPY_DURATION = 2
+export var SELF_SLIPPY_DURATION = 1.5
 
 
 var condiment_residue = preload("res://Spells/Spell Extras/CondimentResidue.tscn")
@@ -28,3 +29,12 @@ func animation_finished():
 func on_start():
 	var _obj = $AnimatedSprite.connect("animation_finished", self, "animation_finished")
 	$AnimatedSprite.play("start")
+
+
+func try_self_cast(player):
+	if player.mana < MANA_COST: 
+		queue_free()
+		return
+	
+	player.mana -= MANA_COST
+	player.Afflict(Effects.slippy, SELF_SLIPPY_DURATION)
