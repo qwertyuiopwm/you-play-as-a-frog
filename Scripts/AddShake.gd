77932b89330e038f307f91f01471dd4eef52c4e0
Oshawhat <alannobledalL@gmail.com>
2024-03-15 08:31:08 -0500

    capitalize pickupable

--- Scripts/AddShake.gd
+++ Scripts/AddShake.gd
@@ -1,11 +1,11 @@
 extends "res://Scripts/FreeOnTrigger.gd"
 
 
-var pickupable = false setget pickupable_set, pickupable_get
+var Pickupable = false setget pickupable_set, pickupable_get
 
 func pickupable_set(val):
-	pickupable = val
+	Pickupable = val
 	$PlayerDetector.Enabled = false
 	
 func pickupable_get():
-	return pickupable
+	return Pickupable
