b9ae05c4a3b7092d7fb30c1e2e65fc4cebe305e4
Oshawhat <alannobledalL@gmail.com>
2024-03-16 13:34:04 -0500

    add is_sliding and has_effect methods to entity

--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -8,10 +8,16 @@ var sliding = false
 var can_move = true
 
 
+func is_sliding():
+	return sliding or has_effect(Effects.slippy)
+
+
 func Afflict(effect, duration: float=-1):
 	if effect in IMMUNITIES: return
 	$EffectManager.Afflict(effect, duration)
 
-
 func Cure(effect):
 	$EffectManager.Cure(effect)
+
+func has_effect(effect):
+	return $EffectManager.has_effect(effect)
