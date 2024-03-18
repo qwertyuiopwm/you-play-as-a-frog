78f418741122534d66e4c880cd68671a4be67d8f
Oshawhat <alannobledalL@gmail.com>
2024-03-18 10:33:57 -0500

    dont free enemy manager until it has had an enemy childed to it

--- Scripts/EnemyManager.gd
+++ Scripts/EnemyManager.gd
@@ -1,5 +1,19 @@
 extends "res://Scripts/Trigger.gd"
 
 
+var waiting_until_enemy
+
+
+func _ready():
+	if condition():
+		waiting_until_enemy = true
+
+
+func _process(delta):
+	if waiting_until_enemy and  get_child_count() > 0:
+		waiting_until_enemy = false
+
+
 func condition():
-	return get_child_count() == 0
+	return get_child_count() == 0 \
+		   and not waiting_until_enemy
