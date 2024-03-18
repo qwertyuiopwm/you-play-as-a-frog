d56e3ab90fae21622480fc163aa290f8f19f3ae0
Oshawhat <alannobledalL@gmail.com>
2024-03-18 10:05:32 -0500

    child enemy to parent

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -5,8 +5,6 @@ export(Array, PackedScene) var Enemies
 export(NodePath) var SpawnPos
 export var SpawnRadius := 0.0
 
-onready var main = get_node("/root/Main")
-
 
 func onTriggerAny(trigger):
 	for enemy in Enemies:
@@ -18,6 +16,6 @@ func onTriggerAny(trigger):
 			pos_offset.x = ((randf() * 2) - 1) * SpawnRadius
 			pos_offset.y = ((randf() * 2) - 1) * SpawnRadius
 		
-		main.call_deferred("add_child", enemy)
+		get_parent().call_deferred("add_child", enemy)
 		
 		enemy.global_position = enemy_pos + pos_offset
