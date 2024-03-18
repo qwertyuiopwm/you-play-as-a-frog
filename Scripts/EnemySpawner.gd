64efeef839868390080b2c22a506c963c00533e5
Oshawhat <alannobledalL@gmail.com>
2024-03-18 10:02:57 -0500

    set position of spawned enemy

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -2,6 +2,8 @@ extends "res://Scripts/Triggerable.gd"
 
 
 export(Array, PackedScene) var Enemies
+export(NodePath) var SpawnPos
+export var SpawnRadius := 0.0
 
 onready var main = get_node("/root/Main")
 
@@ -10,4 +12,11 @@ func onTriggerAny(trigger):
 	for enemy in Enemies:
 		enemy.instance()
 		
+		var enemy_pos = get_node(SpawnPos.global_position)
+		var pos_offset = Vector2.ZERO
+		pos_offset.x = ((randf() * 2) - 1) * SpawnRadius
+		pos_offset.y = ((randf() * 2) - 1) * SpawnRadius
+		
 		main.call_deferred("add_child", enemy)
+		
+		enemy.global_position = enemy_pos + pos_offset
