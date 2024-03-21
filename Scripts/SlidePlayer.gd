90b2933addbe13a606167a44a6b749b64182cbae
Oshawhat <alannobledalL@gmail.com>
2024-03-21 16:28:22 -0500

    create slide object and slide player

---
+++ Scripts/SlidePlayer.gd
@@ -0,0 +1,25 @@
+extends "res://Scripts/Triggerable.gd"
+
+
+export(float) var SlideTime
+export(NodePath) var TargetPos
+
+onready var Player = get_node("/root/Main/Player")
+
+
+func _ready():
+	$Tween.connect("tween_completed", self, "tween_done")
+
+
+func onTriggerAny(trigger):
+	Player.can_move = false
+	$Tween.interpolate_property(Player, "position", 
+		Player.global_position, 
+		TargetPos.global_position, 
+		SlideTime, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+	)
+	$Tween.start()
+
+
+func tween_completed():
+	Player.can_move = true
