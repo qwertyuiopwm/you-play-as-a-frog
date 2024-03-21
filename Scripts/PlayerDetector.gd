1bf3fb2655b9d6e11b9d4e7ae637359235862549
Oshawhat <alannobledalL@gmail.com>
2024-03-21 10:27:54 -0500

    fix warnings

--- Scripts/PlayerDetector.gd
+++ Scripts/PlayerDetector.gd
@@ -7,8 +7,8 @@ export var InverseSignal = false
 
 
 func _init():
-	connect("body_entered", self, "_on_PlayerDetector_body_entered")
-	connect("body_exited", self, "_on_PlayerDetector_body_exited")
+	var _c = connect("body_entered", self, "_on_PlayerDetector_body_entered")
+	var __c = connect("body_exited", self, "_on_PlayerDetector_body_exited")
 
 
 func _on_PlayerDetector_body_entered(body):
