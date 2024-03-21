2647ff5c7356ef1b1ad251d7e4f53a89a4c162fa
Oshawhat <alannobledalL@gmail.com>
2024-03-21 08:47:12 -0500

    update scrolling spell

--- Scroll.gd
+++ Scroll.gd
@@ -2,14 +2,32 @@ extends ScrollContainer
 
 
 export var PIXEL_PER_SECOND = 10
+export var FADE_DELAY: float = 5
+export var FADE_DURATION: float = 5
 
 var scroll: float
 
+enum states {
+	SCROLLING,
+	FADING,
+	DONE,
+}
+
+var state = states.SCROLLING
+
 
 func _process(delta):
-	visible = get_parent().get_parent().visible
-	
-	if not visible: return
-	scroll += PIXEL_PER_SECOND * delta
-	print(scroll_vertical, " ", scroll)
-	scroll_vertical = int(scroll)
+	match state:
+		states.SCROLLING:
+			visible = get_parent().get_parent().visible
+			
+			if not visible: return
+			scroll += PIXEL_PER_SECOND * delta
+			scroll_vertical = int(scroll)
+			if scroll > scroll_vertical + (PIXEL_PER_SECOND * FADE_DELAY):
+				state = states.FADING
+		
+		states.FADING:
+			modulate.a -= (1.0 / FADE_DURATION) * delta
+			if modulate.a >= 0: return
+			state = states.DONE
