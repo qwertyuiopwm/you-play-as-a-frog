23e4f46e0b61a1031cfe214ac61a4872c30a29a6
Oshawhat <alannobledalL@gmail.com>
2024-03-21 03:09:10 -0500

    add scrolling script for credits

---
+++ Scroll.gd
@@ -0,0 +1,15 @@
+extends ScrollContainer
+
+
+export var PIXEL_PER_SECOND = 10
+
+var scroll: float
+
+
+func _process(delta):
+	visible = get_parent().get_parent().visible
+	
+	if not visible: return
+	scroll += PIXEL_PER_SECOND * delta
+	print(scroll_vertical, " ", scroll)
+	scroll_vertical = scroll
