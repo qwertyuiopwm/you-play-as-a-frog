b4644d347d3b4296155e0d0c6410de17aed89c13
Oshawhat <alannobledalL@gmail.com>
2024-04-17 11:44:26 -0500

    add delay to pass trigger

--- Scripts/PassTrigger.gd
+++ Scripts/PassTrigger.gd
@@ -2,9 +2,12 @@ extends "res://Scripts/Triggerable.gd"
 
 
 export(Array, NodePath) var Triggerables
+export var delay: float = 0
 
 
 func onTriggerAny(trigger):
+	if delay > 0:
+		yield(Main.wait(delay), "completed")
 	for _triggerable in Triggerables:
 		var triggerable = get_node(_triggerable)
 		triggerable.trigger(trigger)
