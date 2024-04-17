99c49706c6ec8566a89b21b35510f08183297bfb
Oshawhat <alannobledalL@gmail.com>
2024-04-17 11:45:46 -0500

    disable collider when not pickupable

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -5,10 +5,19 @@ enum type {
 	Small = 1,
 }
 
-export var Pickupable := true
+export var Pickupable := true setget setPickupable, getPickupable
 export(type) var Type = type.Big
 
 onready var Main = get_node("/root/Main")
+
+
+func setPickupable(val):
+	Pickupable = val
+	$StaticBody2D/CollisionShape2D.disabled = !val
+	
+func getPickupable():
+	return Pickupable
+
 
 func on_pickup(_player):
 	pass
