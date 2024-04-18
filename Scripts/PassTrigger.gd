67431fae6756ee8f178b9b8a1e03cdd323915157
Oshawhat <alannobledalL@gmail.com>
2024-04-18 18:05:39 -0500

    add inverse option to pass trigger

--- Scripts/PassTrigger.gd
+++ Scripts/PassTrigger.gd
@@ -2,12 +2,13 @@ extends "res://Scripts/Triggerable.gd"
 
 
 export(Array, NodePath) var Triggerables
-export var delay: float = 0
+export var Delay: float = 0
+export var InvertSignal := false
 
 
 func onTriggerAny(trigger):
-	if delay > 0:
-		yield(Main.wait(delay), "completed")
+	if Delay > 0:
+		yield(Main.wait(Delay), "completed")
 	for _triggerable in Triggerables:
 		var triggerable = get_node(_triggerable)
-		triggerable.trigger(trigger)
+		triggerable.trigger((!trigger) if InvertSignal else trigger)
