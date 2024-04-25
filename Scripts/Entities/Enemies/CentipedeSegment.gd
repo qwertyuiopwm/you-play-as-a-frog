a396f37c227556caaaf291f548b3941d3278f0cc
Oshawhat <alannobledall@gmail.com>
2024-04-25 14:25:39 -0500

    apply slip to next segment if already slippy

--- Scripts/Entities/Enemies/CentipedeSegment.gd
+++ Scripts/Entities/Enemies/CentipedeSegment.gd
@@ -7,14 +7,23 @@ var next_segment
 var head
 
 
-func hurt(damage: float):
-	head.hurt(damage)
-
-
 func _ready():
+	$AnimatedSprite.playing = true
 	global_position = next_segment_back.global_position - $Front.position.rotated(rotation)
 
 
 func _physics_process(_delta):
 	rotation = global_position.angle_to_point(next_segment_back.global_position)
 	global_position = next_segment_back.global_position - $Front.position.rotated(rotation)
+
+
+func hurt(damage: float):
+	head.hurt(damage)
+
+
+func Afflict(effect, duration: float = -1, override_immunities := false):
+	if has_effect(effect) and not next_segment.has_effect(effect):
+		next_segment.Afflict(effect, duration, override_immunities)
+		return
+		
+	.Afflict(effect, duration, override_immunities)
