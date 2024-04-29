98a974e3c48a0b84e9aa72b3e35cc08ba2d5e281
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-04-29 14:30:20 -0500

    Add status effects to HP bar

--- Scripts/GUI/HPBar.gd
+++ Scripts/GUI/HPBar.gd
@@ -7,3 +7,8 @@ onready var parent = get_parent()
 func _process(_delta):
 	$TextureProgress.max_value = parent.maxHealth
 	$TextureProgress.value = parent.health
+	
+	$slipped.visible = parent.has_effect(Effects.slippy)
+	# TODO: implement slowed effect
+	#$slowed.visible = parent.has_effect(Effects.slowed)
+	$poisoned.visible = parent.has_effect(Effects.poison)
