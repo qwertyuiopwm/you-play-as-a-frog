1293017a21de0a1743bbd2975c306b34a339bbe4
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-05-09 12:49:56 -0500

    Add explosion spell (No icon yet)

--- Scripts/Spells.gd
+++ Scripts/Spells.gd
@@ -5,10 +5,12 @@ const Procure_Condiment = preload("res://Spells/ProcureCondiment.tscn")
 const Dev_Beam = preload("res://Spells/DevBeam.tscn")
 const Shake = preload("res://Spells/Shake.tscn")
 const PoisonBubble = preload("res://Spells/PoisonBubble.tscn")
+const Explode = preload("res://Spells/Explode.tscn")
 
 const AllSpells = {
 	Procure_Condiment = Procure_Condiment,
 	Dev_Beam = Dev_Beam,
 	Shake = Shake,
 	PoisonBubble = PoisonBubble,
+	Explode = Explode,
 }
