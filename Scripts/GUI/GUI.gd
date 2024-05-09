8a29b4466344e5c8357ff2e0f8e5ab9b3fe80009
Oshawhat <alannobledalL@gmail.com>
2024-05-09 09:30:29 -0500

    play music through changemusic triggerable

--- Scripts/GUI/GUI.gd
+++ Scripts/GUI/GUI.gd
@@ -246,7 +246,7 @@ func _process(_delta):
 
 
 func _on_play_pressed():
-	MusicPlayer.PlaySong("ForestMusic")
+	$StartMusic.trigger(true)
 	if not SaveSys.saveExists():
 		SaveSys.save()
 	Main.pause(false, [])
