f0c3a647b5e9b531a41f29dc9b6d8e514df1f466
Oshawhat <alannobledalL@gmail.com>
2024-05-09 14:27:52 -0500

    create crocodile

---
+++ Scripts/Entities/Enemies/Crocodile.gd
@@ -0,0 +1,54 @@
+extends "res://Scripts/BaseScripts/Enemy.gd"
+
+
+export var BREAK_DELAY: float = 1
+export var CONTACT_DAMAGE: float = 40
+export var KNOCKBACK_VELOCITY: Vector2 = Vector2(-200, 0)
+
+
+enum states {
+	MOVING,
+	BREAKING,
+}
+
+
+var state = states.MOVING
+
+
+func _ready():
+	var _c = $HitCollider.connect("body_entered", self, "HitCollider_body_entered")
+
+
+func on_death():
+	$AnimatedSprite.play("death")
+	yield($AnimatedSprite, "animation_finished")
+	emit_signal("death_finished")
+
+
+func _physics_process(delta):
+	move(global_position + Vector2(-1, 0), delta)
+	if state == states.BREAKING:
+		return
+	
+	for body in $BreakTiles.get_overlapping_bodies():
+		if body is TileMap:
+			state = states.BREAKING
+			break_tiles()
+			break
+
+
+func break_tiles():
+	print("waiting...")
+	yield(Main.wait(1), "completed")
+	print("breaking")
+	$BreakTiles.break_tiles()
+	state = states.MOVING
+
+
+func HitCollider_body_entered(body):
+	if not body.is_in_group("Player"):
+		return
+	
+	body.hurt(CONTACT_DAMAGE)
+	body.Afflict(Effects.slippy, 1)
+	body.velocity = KNOCKBACK_VELOCITY
