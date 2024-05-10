7e9ef01ec7cd09ebac31453045cecc81600157eb
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-05-10 12:12:44 -0500

    merge

--- Scripts/Entities/Enemies/SmallSpider.gd
+++ Scripts/Entities/Enemies/SmallSpider.gd
@@ -1,14 +1,19 @@
 extends "res://Scripts/BaseScripts/Enemy.gd"
 
 
-export var CIRCLE_RADIUS = 60
-export var ATTACK_RADIUS = 70
-export var CONTACT_DAMAGE = 10
-export var JUMP_ROTATION_SPEED = 400
-export var JUMP_DIST = 200
-export var TARGET_COMFORT_RADIUS = 10
-export var DIR_ROT_OFFSET = 30
+export var CIRCLE_RADIUS: int = 60
+export var ATTACK_RADIUS: int = 70
+export var CONTACT_DAMAGE: int = 10
+export var JUMP_ROTATION_SPEED: int = 400
+export var JUMP_DIST: int = 200
+export var TARGET_COMFORT_RADIUS: int = 10
+export var DIR_ROT_OFFSET: int = 30
+export var BABY_SPAWN_DIST: int = 25
+export var BABY_SPIDERS_SPAWNED: int = 0
+export var BabySpiderScene: PackedScene
 
+var rng = RandomNumberGenerator.new()
+var randnum = 0
 
 enum states {
 	STILL,
@@ -17,19 +22,28 @@ enum states {
 	JUMPING,
 }
 
-
 var state = states.STILL
-var rng = RandomNumberGenerator.new()
-var randnum = 0
 
 
 func _ready():
 	var _c = $HitCollider.connect("body_entered", self, "HitCollider_body_entered")
 
+
 func on_death():
 	$AnimatedSprite.play("death")
+	
+	for _x in range(BABY_SPIDERS_SPAWNED):
+		var inst = BabySpiderScene.instance()
+		Main.call_deferred("add_child", inst)
+		
+		var x = rng.randi_range(-BABY_SPAWN_DIST, BABY_SPAWN_DIST)
+		var y = rng.randi_range(-BABY_SPAWN_DIST, BABY_SPAWN_DIST)
+		
+		inst.global_position = global_position + Vector2(x, y)
+	
 	yield($AnimatedSprite, "animation_finished")
 	emit_signal("death_finished")
+
 
 func _physics_process(delta):
 	if health <= 0:
@@ -40,7 +54,6 @@ func _physics_process(delta):
 	if is_sliding():
 		move(0, delta)
 		return
-	
 	
 	match state:
 		states.STILL: 
