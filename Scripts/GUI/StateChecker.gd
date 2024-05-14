6fd039c45934603fe5b806a7a760f81cff0287a7
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-05-14 12:16:59 -0500

    merg

--- Scripts/GUI/StateChecker.gd
+++ Scripts/GUI/StateChecker.gd
@@ -2,13 +2,14 @@ extends Label
 
 
 export var Property: String = "state"
+export var checkedNode: NodePath
 
-onready var parent = get_parent()
-
+onready var checked = get_node(checkedNode) if checkedNode else get_parent()
 
 func _ready():
 	visible = OS.is_debug_build()
 
 
 func _process(_delta):
-	text = str(parent.get(Property))
+	
+	text = str(checked.get(Property))
