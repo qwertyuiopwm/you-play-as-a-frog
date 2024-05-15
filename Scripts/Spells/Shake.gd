fee6e97b761a06f2ab0db029b035ad62c56d3b6e
Oshawhat <alannobledalL@gmail.com>
2024-05-15 09:47:29 -0500

    add var for frame to break on shake

--- Scripts/Spells/Shake.gd
+++ Scripts/Spells/Shake.gd
@@ -1,6 +1,9 @@
 extends "res://Scripts/BaseScripts/InstantSpell.gd"
 
 
+export var FRAME_TO_BREAK: int = 3
+
+
 func on_cast(_caster):
 	var _c = $AnimatedSprite.connect("frame_changed", self, "shake_if_time")
 	var __c = $AnimatedSprite.connect("animation_finished", self, "queue_free")
@@ -8,7 +11,7 @@ func on_cast(_caster):
 
 
 func shake_if_time():
-	if not $AnimatedSprite.frame == 4:
+	if not $AnimatedSprite.frame == FRAME_TO_BREAK:
 		return
 	
 	$BreakTiles.break_tiles()
