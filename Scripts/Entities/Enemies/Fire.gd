9025063475058e489d602d7b5f171bb16a5e8143
qwertyuiopwm <wyatt.mcintosh@students.bsd124.org>
2024-05-20 08:09:34 -0500

    Dragonfly finished (Not balanced)

---
+++ Scripts/Entities/Enemies/Fire.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+
+export var DamagePerSecond = 50
+export var TTLSeconds = 5
+
+var secondsAlive = 0
+
+func _ready():
+	$AnimatedSprite.play("start")
+	yield($AnimatedSprite, "animation_finished")
+	$AnimatedSprite.play("loop")
+	
+func _physics_process(delta):
+	secondsAlive+=delta
+	if secondsAlive >= TTLSeconds:
+		queue_free()
+		return
+	
+	var bodies = $DamageCollider.get_overlapping_bodies()
+	
+	for body in bodies:
+		if not body.is_in_group("Player"):
+			continue
+		body.hurt(DamagePerSecond * delta)
