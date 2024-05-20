d9dfc82db78013aa59e801edaab623f28fe8b432
Oshawhat <alannobledalL@gmail.com>
2024-05-20 08:55:52 -0500

    finally change doors to use tweens

--- Scripts/Triggerables/Door.gd
+++ Scripts/Triggerables/Door.gd
@@ -1,43 +1,37 @@
 extends "res://Scripts/BaseScripts/Triggerable.gd"
 
 
-export var TRIGGER_ON_START = true
-
 var TARGET_COMFORT_RANGE = 5
 
 export var OpenOffset = Vector2(0, 0)
 export var Hide = true
-export var OpenSpeed = -1
+export var OPEN_TIME = 0
 
-var start_pos
+onready var moveTween: Tween = Tween.new()
+onready var start_pos = position
+
 var closed:bool
 
 
 func _ready():
-	start_pos = global_position
+	Main.call_deferred("add_child", moveTween)
 	call_deferred("setup")
 
 
 func setup():
-	if not TRIGGER_ON_START: return
 	trigger(State)
-
-
-func _process(delta):
-	var target_pos = start_pos if closed else global_position + OpenOffset
-	
-	var dist_to = global_position.distance_to(target_pos)
-	if (OpenSpeed <= 0) or (0 < dist_to and dist_to < TARGET_COMFORT_RANGE):
-		global_position = target_pos
-		return
-	
-	if dist_to < TARGET_COMFORT_RANGE: return
-	
-	var dir_to = global_position.direction_to(target_pos)
-	global_position += dir_to * OpenSpeed * delta
 
 
 func onTriggerAny(close: bool):
 	closed = close
 	if Hide:
 		visible = close
+	
+	var target_pos = start_pos if closed else start_pos + OpenOffset
+	
+	var _v = moveTween.interpolate_property(self, "position", 
+		position, 
+		target_pos, 
+		OPEN_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+	)
+	var __v = moveTween.start()
